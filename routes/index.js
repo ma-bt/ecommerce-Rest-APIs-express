bdd83997b199590e40acb2d3d22e754fe12dbc05
Mandira Basnet <mandeerab20@gmail.com>
2023-11-21 13:46:47 +0545

    feat: login api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { registerController } from "../controllers";
+import { loginController, registerController } from "../controllers";
 
 const router = express.Router()
 
 router.post('/register',registerController.register )//registerController is register logic which is written in Controller Folder and /register is an api
-
+router.post('/login',loginController.login)
 export default router
